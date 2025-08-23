08c472b9f3245752f97dfefd5a7bc7d8b7d184e3
Courtney Cary <courtg123@gmail.com>
2025-08-22 17:14:34 -0700

    add google analytics

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,6 +5,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Ever Explorations - Digital Workshop</title>
     
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-1N4CQKCK9H"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'G-1N4CQKCK9H');
+    </script>
+    
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
